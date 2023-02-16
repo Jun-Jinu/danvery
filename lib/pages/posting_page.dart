06404a8533efb245cb1d4dde2c05b5bc36aee5fc
ForwardOoh <soa9719@naver.com>
2023-02-17 02:16:28 +0900

    feat: 게시판 대댓글 추가

--- lib/pages/posting_page.dart
+++ lib/pages/posting_page.dart
@@ -34,7 +34,6 @@ class PostingPage extends StatelessWidget {
           poster: '123',
           content: '여기는 내용이 나올 곳',
           publishDate: " 날짜는 01/15 12:39",
-          imageCnt: 1,
           commentCnt: 3,
           likeCnt: 10,
         ),
@@ -49,7 +48,6 @@ class PostingView extends StatelessWidget {
   final String content;
   final String? publishDate; // null 수정 예정
   final String? imageUrl;
-  final int? imageCnt;
   final int? commentCnt;
   final int? likeCnt;
 
@@ -60,82 +58,114 @@ class PostingView extends StatelessWidget {
       required this.content,
       this.publishDate,
       this.imageUrl,
-      this.imageCnt,
       this.commentCnt,
       this.likeCnt});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
-          child: ListTile(
-            shape: Border(
-              bottom: BorderSide(
-                width: 0.5,
-                color: Palette.lightGrey,
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: ListTile(
+              shape: Border(
+                bottom: BorderSide(
+                  width: 0.5,
+                  color: Palette.lightGrey,
+                ),
+              ),
+              title: Text(title),
+              subtitle: Text(poster +
+                  " " +
+                  (publishDate != null ? publishDate.toString() : "")),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: Text(content),
+          ),
+          Row(
+            children: <Widget>[
+              Wrap(
+                spacing: 12,
+                children: [
+                  commentCnt != null
+                      ? Row(
+                          children: [
+                            Icon(Icons.chat_outlined),
+                            Text(
+                              commentCnt.toString(),
+                              style: TextStyle(
+                                  fontSize: 20, color: Palette.lightGrey),
+                            ),
+                          ],
+                        )
+                      : Row(),
+                  likeCnt != null
+                      ? Row(
+                          children: [
+                            Icon(Icons.thumb_up_off_alt),
+                            Text(
+                              likeCnt.toString(),
+                              style: TextStyle(
+                                  fontSize: 20, color: Palette.lightGrey),
+                            ),
+                          ],
+                        )
+                      : Row(),
+                ],
+              ),
+            ],
+          ),
+          Divider(thickness: 5, color: Palette.brightGrey),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 40.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: Palette.whiteGrey,
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(
+                      title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      content,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          color: Colors.black.withOpacity(0.6), fontSize: 15.0),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            title: Text(title),
-            subtitle: Text(poster +
-                " " +
-                (publishDate != null ? publishDate.toString() : "")),
-            trailing: Icon(Icons.more_vert),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
-          child: Text(content),
-        ),
-        Row(
-          children: <Widget>[
-            Wrap(
-              spacing: 12,
-              children: [
-                imageCnt != null
-                    ? Row(
-                        children: [
-                          Icon(Icons.image_outlined),
-                          Text(
-                            imageCnt.toString(),
-                            style: TextStyle(
-                                fontSize: 20, color: Palette.lightGrey),
-                          ),
-                        ],
-                      )
-                    : Row(),
-                commentCnt != null
-                    ? Row(
-                        children: [
-                          Icon(Icons.chat_outlined),
-                          Text(
-                            commentCnt.toString(),
-                            style: TextStyle(
-                                fontSize: 20, color: Palette.lightGrey),
-                          ),
-                        ],
-                      )
-                    : Row(),
-                likeCnt != null
-                    ? Row(
-                        children: [
-                          Icon(Icons.thumb_up_off_alt),
-                          Text(
-                            likeCnt.toString(),
-                            style: TextStyle(
-                                fontSize: 20, color: Palette.lightGrey),
-                          ),
-                        ],
-                      )
-                    : Row(),
-              ],
-            ),
-          ],
-        ),
-        Divider(thickness: 5, color: Palette.brightGrey)
-      ],
+          )
+        ],
+      ),
     );
   }
 }
